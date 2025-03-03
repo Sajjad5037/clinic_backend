26060cadfe51eb2568140b252943e8318dc7000e
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-03-03 20:44:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -405,7 +405,5 @@ def delete_patient(id: int):
     return {"message": "Patient deleted successfully"}
     
 if __name__ == "__main__":
-    if __name__ == "__main__":
-    
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
